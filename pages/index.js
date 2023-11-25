945b22fde1cbdbe5a51317e62439a1415d84db28
dctoigo <104244310+dctoigo@users.noreply.github.com>
2023-11-25 04:04:48 +0000

    ajustando a primeira frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home () {
-    return <h1>A nanica que roubou meu ❤️, para quem construirei 🏰 e até <br></br>
-                roubaria a 🌝! Só pra você saber: Te ❤️ desse tanto ☝️! <br></br>
+    return <h1>A nanica que roubou meu ❤️, para quem construirei 🏰 e até roubaria a 🌝! Só pra você saber: Te ❤️ desse tanto ☝️! <br></br>
+                <br></br>
+                <br></br>
                 PS: manda um ❤️ se gostou!
         </h1>
 }
