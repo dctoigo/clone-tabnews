09384f6307afeb892fded0e5016353a4a999e6f9
dctoigo <104244310+dctoigo@users.noreply.github.com>
2023-11-25 03:58:43 +0000

    adicinada nova pagina para segunda mensagem

---
+++ pages/tigrada.js
@@ -0,0 +1,7 @@
+function Home () {
+    return <h1>Fala 🐯🐅! Lembrei de vocês enquanto desenvolvo me desenvolvo como desenvolvedor Web 😂! <br></br>
+                Que tal a página? Meu teste drive nalgumas brincadeiras! Manda aí, que nota mereço?
+        </h1>
+}
+
+export default Home;
