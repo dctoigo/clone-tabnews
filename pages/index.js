42a872286b7aa62e4964a38aa8a38714caeee915
dctoigo <104244310+dctoigo@users.noreply.github.com>
2023-12-13 23:50:46 +0000

    adicina nova home para página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      A nanica que roubou meu ❤️, para quem construirei 🏰 e até roubaria a 🌝!
-      Só pra você saber: Te ❤️ desse tanto ☝️! <br></br>
-      <br></br>
-      <br></br>
-      PS: manda um ❤️ se gostou!
-    </h1>
-  );
+  return <h1></h1>;
 }
 
 export default Home;
