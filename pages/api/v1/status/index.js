4dc574dbd36abc9eb2e4c727fee55ee975581d98
dctoigo <104244310+dctoigo@users.noreply.github.com>
2023-12-15 03:44:05 +0000

    add `compose` config file with database service

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,5 @@
 function status(request, response) {
-  response
-    .status(200)
-    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
+  response.status(200).json({ chave: "são acima da média" });
 }
 
 export default status;
